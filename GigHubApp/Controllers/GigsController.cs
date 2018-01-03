7a119c48074091933cf1990f0c63d0a48d9dccb0
Jhonny Horacio Crespo <jhonny.h.cr@gmail.com>
2018-01-03 18:51:35 -0400

    Move DateTime calculation to GigFormViewModel

--- GigHubApp/Controllers/GigsController.cs
+++ GigHubApp/Controllers/GigsController.cs
@@ -1,7 +1,6 @@
 ﻿using GigHubApp.Models;
 using GigHubApp.ViewModels;
 using Microsoft.AspNet.Identity;
-using System;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -36,7 +35,8 @@ namespace GigHubApp.Controllers
             var gig = new Gig
             {
                 ArtistId = User.Identity.GetUserId(),
-                DateTime = DateTime.Parse(string.Format("{0} {1}", viewModel.Date, viewModel.Time)),
+                //DateTime = DateTime.Parse(string.Format("{0} {1}", viewModel.Date, viewModel.Time)),
+                DateTime = viewModel.DateTime,
                 GenreId = viewModel.Genre,
                 Venue = viewModel.Venue
             };
--- GigHubApp/ViewModels/GigFormViewModel.cs
+++ GigHubApp/ViewModels/GigFormViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using GigHubApp.Models;
+using System;
 using System.Collections.Generic;
 
 namespace GigHubApp.ViewModels
@@ -10,5 +11,12 @@ namespace GigHubApp.ViewModels
         public string Time { get; set; }
         public byte Genre { get; set; }
         public IEnumerable<Genre> Genres { get; set; }
+        public DateTime DateTime
+        {
+            get
+            {
+                return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+            }
+        }
     }
 }
