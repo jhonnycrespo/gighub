48330a09c17534adc7522ef1094b02d77511150e
Jhonny Horacio Crespo <jhonny.h.cr@gmail.com>
2018-01-03 16:12:08 -0400

    Add project files.

---
+++ GigHubApp/Models/GigHubContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+
+namespace GigHubApp.Models
+{
+    public class GigHubContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Gig> Gigs { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+        public GigHubContext()
+            : base("GigHubContext", throwIfV1Schema: false)
+        {
+        }
+
+        public static GigHubContext Create()
+        {
+            return new GigHubContext();
+        }
+    }
+}
