7d836f2c4d2c83be5f292b144fe63ffaacd5acae
Jhonny Horacio Crespo <jhonny.h.cr@gmail.com>
2018-01-04 17:22:05 -0400

    Implement My Upcoming Gigs page.

--- GigHubApp/Controllers/GigsController.cs
+++ GigHubApp/Controllers/GigsController.cs
@@ -1,6 +1,7 @@
 ﻿using GigHubApp.Models;
 using GigHubApp.ViewModels;
 using Microsoft.AspNet.Identity;
+using System;
 using System.Data.Entity;
 using System.Linq;
 using System.Web.Mvc;
@@ -14,6 +15,18 @@ namespace GigHubApp.Controllers
         public GigsController()
         {
             _context = new GigHubContext();
+        }
+
+        [Authorize]
+        public ActionResult Mine()
+        {
+            var userId = User.Identity.GetUserId();
+            var gigs = _context.Gigs
+                .Where(g => g.ArtistId == userId && g.DateTime > DateTime.Now)
+                .Include(g => g.Genre)
+                .ToList();
+
+            return View(gigs);
         }
 
         [Authorize]
@@ -73,7 +86,7 @@ namespace GigHubApp.Controllers
             _context.Gigs.Add(gig);
             _context.SaveChanges();
 
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Mine", "Gigs");
         }
     }
 }
