2c23425d0e999920bc9bd25048b2bcb061b97a0b
Jhonny Horacio Crespo <jhonny.h.cr@gmail.com>
2018-01-04 13:35:04 -0400

    Hide Buttons if user is not logged in

---
+++ GigHubApp/ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using GigHubApp.Models;
+using System.Collections.Generic;
+
+namespace GigHubApp.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Gig> UpcomingGigs { get; set; }
+        public bool ShowActions { get; set; }
+    }
+}
