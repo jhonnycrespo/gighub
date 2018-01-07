4cf3b085c559ab00bfd8f3ba0de8d48db0e0bd89
Jhonny Horacio Crespo <jhonny.h.cr@gmail.com>
2018-01-06 20:08:19 -0400

    Refactor attendance to a gig

--- GigHubApp/App_Start/BundleConfig.cs
+++ GigHubApp/App_Start/BundleConfig.cs
@@ -7,6 +7,12 @@ namespace GigHubApp
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                        "~/Scripts/app/services/attendanceService.js",
+                        "~/Scripts/app/controllers/gigsController.js",
+                        "~/Scripts/app/app.js"
+            ));
+
             bundles.Add(new ScriptBundle("~/bundles/lib").Include(
                         "~/Scripts/jquery-{version}.js",
                         "~/Scripts/underscore-min.js",
---
+++ GigHubApp/Scripts/app/services/attendanceService.js
@@ -0,0 +1,22 @@
+﻿// revealing module pattern
+var AttendanceService = function () {
+    var createAttendance = function (gigId, done, fail) {
+        $.post("/api/attendances", { gigId: gigId })
+            .done(done)
+            .fail(fail);
+    };
+
+    var deleteAttendance = function (gigId, done, fail) {
+        $.ajax({
+            url: '/api/attendances/' + gigId,
+            method: 'DELETE'
+        })
+            .done(done)
+            .fail(fail);
+    };
+
+    return {
+        createAttendance: createAttendance,
+        deleteAttendance: deleteAttendance
+    };
+}();
