b386871f1a2b398828ecb15474e3ed4840e56f5e
Jhonny Horacio Crespo <jhonny.h.cr@gmail.com>
2018-01-15 13:15:28 -0400

    Add Integration Tests

---
+++ GigHub.IntegrationTests/Controllers/API/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GigHub.IntegrationTests.Controllers.API
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
